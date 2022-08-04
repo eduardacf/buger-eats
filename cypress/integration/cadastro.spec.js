62232cff4e7b3774410933673defe40f4d1dc85d
Eduarda Correia Ferreira <ferreira_eduarda@sicredi.com.br>
2022-08-04 18:01:35 -0300

    .

---
+++ cypress/integration/cadastro.spec.js
@@ -0,0 +1,47 @@
+describe('Cadastro', () => {
+    it('Usuário deve se tornar um entregador', () => {
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var entregador = {
+
+            nome: 'Fernando Papito',
+            cpf: '00001247842',
+            email: 'papito@hotmail.com',
+            whatsapp: '1151998586325',
+            endereco: {
+                cep: '04534011',
+                rua: 'Rua Joaquim Floriano',
+                numero: '1000',
+                complemento: 'Ap 142',
+                bairro: 'Itaim Bibi',
+                cidade_uf: 'São Paulo/SP'
+            },
+            metodo_entrega: 'Moto',
+            cnh: 'cnh-digital.jpg'
+        }
+        cy.get('input[name="name"]').type(entregador.nome)
+        cy.get('input[name="cpf"]').type(entregador.cpf)
+        cy.get('input[name="email"]').type(entregador.email)
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[type=button][value="Buscar CEP"]', { timeout: 7000 }).click()
+
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+
+        cy.get('input[name="address"').should('have.value', entregador.endereco.rua)
+        cy.get('input[name="district"').should('have.value', entregador.endereco.bairro)
+        cy.get('input[name="city-uf"').should('have.value', entregador.endereco.cidade_uf)
+
+        cy.contains('.delivery-method li', entregador.metodo_entrega).click();
+
+        cy.get('input[accept^="image"]').attachFile('/images/' +entregador.cnh)
+        cy.get('form button[type="submit"]'.click()
+
+
+    })
+})
